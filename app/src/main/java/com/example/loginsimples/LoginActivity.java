abdcc962718b406ee5280dfc82732bfaf08b3195
o0Junny0o <jujup1209@gmail.com>
2024-09-26 19:51:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/loginsimples/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.loginsimples;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToLogin(View view) {
+        Intent in = new Intent(LoginActivity.this, PrincipalActivity.class);
+        startActivity(in);
+    }
+}
