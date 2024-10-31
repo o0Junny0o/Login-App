06e9a1ad2e4927db2b7e69512a9d3348b3ae1d2b
o0Junny0o <jujup1209@gmail.com>
2024-10-31 20:27:16 -0300

    :lipstick: feat: atualizando design

--- app/src/main/java/com/example/loginsimples/Note.java
+++ app/src/main/java/com/example/loginsimples/Note.java
@@ -1,8 +1,6 @@
 package com.example.loginsimples;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PrincipalActivity extends AppCompatActivity {
+public class Note extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_note);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void AddNote(View view) {
-        Intent intent = new Intent(PrincipalActivity.this, Note.class);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/loginsimples/NoteDataClass.java
@@ -0,0 +1,4 @@
+package com.example.loginsimples;
+
+public class NoteDataClass {
+}
